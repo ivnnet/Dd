f2d876d500f89a88b9bbe4c56ebf98133f0bd4a1
dropa <altfortts@gmail.com>
2026-07-23 13:35:33 +0300

    Update config loading mechanism
    Refactor configuration loading to support environment variables.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const config = require('./config');
+const config = {};
+
+for (const [key, value] of Object.entries(configFile)) {
+  if (typeof value === "string" && value.startsWith("ENV:")) {
+    config[key] = process.env[value.replace("ENV:", "")];
+  } else {
+    config[key] = value;
+  }
+};
+
 
 const client = new Client({
   intents: [
